45982b67f6669f00096f06d1d4c9677ca8d5c26e
kabouter <Lolbrol5104>
2023-02-15 12:17:35 +0100

    commit

--- src/webparts/fluent/components/Fluent.tsx
+++ src/webparts/fluent/components/Fluent.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import { IFluentProps } from './IFluentProps';
 //import { Web } from "@pnp/sp/webs";
-
-
-
-
 import { getSP } from "../pnpjsConfig";
 import "@pnp/graph/users";
 
@@ -12,17 +8,13 @@ import "@pnp/graph/users";
 
 export default class Fluent extends React.Component<IFluentProps,{}> {
 
- 
 
 
   constructor(props: IFluentProps) {
     super(props);
-    // set initial state
-    
+    // set initial state  
     
   }
-
-
 
 
 public componentDidMount(): void {
@@ -39,26 +31,12 @@ public render(): JSX.Element {
 
 private printCurrentUser = async (): Promise<void> => {
  
-    // do PnP JS query, some notes:
-    //   - .expand() method will retrive Item.File item but only Length property
-    //   - .get() always returns a promise
-    //   - await resolves proimises making your code act syncronous, ergo Promise<IResponseItem[]> becomes IResponse[]
-
-    //Extending our sp object to include caching behavior, this modification will add caching to the sp object itself
-    //this._sp.using(Caching("session"));
-
-    //Creating a new sp object to include caching behavior. This way our original object is unchanged.
     try {
       const currentUser = await getSP.me();
       console.log(`current User: ${currentUser}`);
     } catch (error) {
       
     }
-    
-
-    
-    // Add the items to the state
-    
  
 }
 }
